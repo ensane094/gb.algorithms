d53b19159768bbe4a02b58fd0168fbc0b264db0c
Ensane <Ensane094@gmail.com>
2021-11-02 00:59:41 +0300

    урок 3 первый пункт

---
+++ src/test/java/Lesson3/Main.java
@@ -0,0 +1,18 @@
+package Lesson3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    static String reverseRead (String string){
+        StringBuilder reverse = new StringBuilder();
+        for (int i = string.length()-1; i >= 0; i--) {
+            reverse.append(string.charAt(i));
+        }
+        String finalReverse = reverse.toString();
+        return finalReverse;
+    }
+}
